3a5613c76c62a39cac234c7578a38c984bf31eb0
hanwei <zhouxin@xzgoddess.comn>
2020-11-27 17:06:04 +0800

    增加alibaba数据库连接池配置

--- src/main/java/bcu/edu/bysj/controller/LoginController.java
+++ src/main/java/bcu/edu/bysj/controller/LoginController.java
@@ -2,20 +2,18 @@ package bcu.edu.bysj.controller;
 
 import bcu.edu.bysj.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LoginController {
 
     private LoginService loginService;
 
     @RequestMapping("/login")
         public String login(){
-
-        return"ok";
+        return "login.html";
     }
-
-
 
 }
